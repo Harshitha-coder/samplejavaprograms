834b325af8db475a600ff4e23e87384c6bbe3339
Harshitha1308 <harshithak794@gmail.com>
2021-04-28 20:05:28 +0530

    adding Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,24 @@
+class Datatype{
+
+ public static void main( String[] args)
+{
+byte g=12;
+int i=16;
+short s=8;
+long l=45;
+float f=12.676546f;
+double d=11.5436346;
+String w="XYZ";
+boolean b=true;
+char c='a';
+System.out.println(" int: "+ i);
+System.out.println(" byte: "+ g);
+System.out.println(" short: "+ s);
+System.out.println(" long: "+ l);
+System.out.println(" float: "+ f);
+System.out.println(" double: "+ d);
+System.out.println(" boolean: "+ b);
+System.out.println(" char: "+ c);
+System.out.println(" string: "+ w);
+}
+}
