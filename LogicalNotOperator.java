9da481e06ec888928cad3ffc3c96373deb70c3c2
Harshitha1308 <harshithak794@gmail.com>
2021-04-29 10:05:35 +0530

    add LogicalNotOperator

---
+++ LogicalNotOperator.java
@@ -0,0 +1,14 @@
+class LogoicalNotOperator{
+
+public static void main(String[] args){
+
+boolean condition=true;
+
+boolean notResult= !condition;
+
+System.out.println( condition);//true
+System.out.println( notResult);//false
+}
+}
+
+
