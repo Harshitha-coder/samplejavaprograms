a871433aef147b2353a8823957dc09377d1cad11
Harshitha1308 <harshithak794@gmail.com>
2021-04-29 10:08:23 +0530

    add RelationalOperator

---
+++ RelationalOperator.java
@@ -0,0 +1,20 @@
+class RelationalOperator{
+
+public static void main(String[] args){
+
+int a=10;
+int b=20;
+int c=a;
+ 
+System.out.println(a==b);//false
+System.out.println(a==c);//true
+System.out.println(a!=b);// true
+System.out.println(a<b);//true
+System.out.println(a<=c);//true
+System.out.println(b>c);//true
+System.out.println(a>=b);//false
+
+}
+}
+
+
