eb06d846cb83afb5506a74cfe0d3fc50394e95e5
Harshitha1308 <harshithak794@gmail.com>
2021-04-29 09:55:46 +0530

    add ArithematicOperator

---
+++ ArithematicOperator.java
@@ -0,0 +1,28 @@
+class ArithematicOperator{
+
+public static void main(String[] args){
+
+int a=15;
+int b=24;
+System.out.println(a);
+System.out.println(b);
+ 
+int addResult=a+b;
+System.out.println( "a+b : " + addResult);
+
+int subResult=b-a;
+System.out.println( "b-a : " + subResult);
+
+int mulResult=a*b;
+System.out.println( "a*b : " + mulResult);
+
+int divResult=b/a;
+System.out.println( "b/a : " + divResult);
+
+int modResult=b%a;
+System.out.println( "b%a : " + modResult);
+}
+}
+
+
+
