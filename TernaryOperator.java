fc92a21034a5804341bff0d980db4f37caf2acae
Harshitha1308 <harshithak794@gmail.com>
2021-05-03 11:54:28 +0530

    add TernaryOperator

---
+++ TernaryOperator.java
@@ -0,0 +1,19 @@
+class TernaryOperator{
+
+public static void main(String[] args){
+
+int p = 50;
+
+int q = (p == 20) ? 15 : 30 ;
+
+System.out.println("the value of q is : " + q);
+
+int r = (q == 50) ? 20 : 10 ;
+
+System.out.println("the value of r is : " + r);
+
+}
+
+}
+
+
