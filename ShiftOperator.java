0836b4a2e6908be0a4a4f6d53b7c5f9038725b90
Harshitha1308 <harshithak794@gmail.com>
2021-05-03 20:18:20 +0530

    add ShiftOperator

---
+++ ShiftOperator.java
@@ -0,0 +1,17 @@
+class ShiftOperator{
+
+public static void main(String[] args){
+
+int num1=4;
+int num2=-4;
+int num3=2;
+
+System.out.println( num1<<2); //16
+System.out.println( num2 >>2);//-1
+System.out.println( num3>>>1);//1
+System.out.println( num3>>3);//0
+
+}
+}
+
+
